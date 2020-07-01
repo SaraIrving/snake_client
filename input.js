8093b5dca55d77c9e63569b0291eacc5dcea9d9a
Sara Irving <sarakirving@gmail.com>
2020-07-01 21:00:55 +0000

    Updated handleUserInput to react to the w,a,s,d keys which we will later link to movement commands. Also modified setupInput to accept the connection returned from connct() as a parameter so that it has a connection to the server which will allow it to send the movement commands to the server

--- input.js
+++ input.js
@@ -3,19 +3,33 @@
  * Specifically, so that we can handle user input via stdin
  */
 
+ // Stores the active TCP connection object.
+let connection;
+
+
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === 'w') {
+    console.log("up")
+  } else if (data === 'a') {
+    console.log("left")
+  } else if (data === 's') {
+    console.log("down")
+  } else if (data === 'd') {
+    console.log("right")
   }
 };
 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput)
+
 
   return stdin;
 };
