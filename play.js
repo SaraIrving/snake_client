a486f43fc4f74e306a450604cf3b400a5f3bb1a9
Sara Irving <sarakirving@gmail.com>
2020-07-01 20:25:45 +0000

    Added setupInput function which listens for input from the keyboard and created handleUserInput funciton which exits the game if the user types Ctrl+C

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const connect = require('./client').connect;
 console.log('Connecting ...');
 connect();
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+};
+setupInput();
